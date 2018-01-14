ffaabe29366c1d882f9080fff75fdaafe3860b92
Tensai75 <34959627+Tensai75@users.noreply.github.com>
2018-01-14 16:35:36 +0100

    Again some heavy rewrite of the code to extract the information from the selected text
    Heavy rewrite of the code to extract the information from the selected text using regex expressions from the whole selection without splitting and looping it line by line.
    See comments in the source code for detailed explanations.
    Also did some formal cleaning of the code.

--- overlay.js
+++ overlay.js
@@ -4,46 +4,64 @@ var NZBselection = "",
 	NZBheader = "",
 	NZBpassword = "",
 	NZBgroup = "",
-	NZBlink = "",
-	lines = "";
+	NZBlink = "";
 
-// get html source from selected text and replace input tags by their value
-NZBselection = getSelectionHtml().replace(/<input.+?value\s*?=\s*?['"](.*?)['"].*?>/gi, '$1');
+// get html source from the selected text and replace input tags by their value
+// encapsulate the value with line breaks to make sure it is on a single line
+NZBselection = getSelectionHtml().replace(/<input.+?value\s*?=\s*?['"](.*?)['"].*?>/ig, "\n$1\n");
+
+// add some line breaks to some ending tags to avoid text rendered on different lines to be joined in one line
+NZBselection = NZBselection.replace(/(<\/div>|<\/span>|<\/p>|<\/td>|<\/li>)/ig, "$1\n");
 
 // remove all tags, blank lines and leading/trailing spaces
-NZBselection = jQuery(NZBselection).text().replace(/^\s*([\s|\S]*?)\s*?$/gm, '$1');
+NZBselection = jQuery(NZBselection).text().replace(/^\s*([\s|\S]*?)\s*?$/mg, "$1");
 
-// split text per line into an array
-lines = NZBselection.split("\n");
+// test if the selection contains a description for the header starting with some common words used for and ending with a colon
+if (/^(header|subje[ck]t|betreff).*:\s*/im.test(NZBselection)) {
+	// set the header to the text after the description
+	// we search for any text until we find it and then get all of it until the next line break
+	// like this we will find the header information either if placed on the same line or if placed on the next line
+	// we also take care of if the description is used twice (e.g. before the hidden tag and in the hidden tag again)
+	NZBheader = NZBselection.match(/^((header|subje[ck]t|betreff).*:\s*)+(\S.*\S)[\n|\r]/im)[3];
+}
 
-// set the title to the first line, replace spaces with periods and remove quotation marks
-NZBtitle = lines[0].replace(/[\s]/g, ".").replace(/['"]/g, "");
-
-// loop through the further lines to get header and password
-for (i = 1; i < lines.length; i++) {
-	if (lines[i].match(/^(head[er]{0,2}|subje[ck]t)\s*:*\s*/i)) {
-		NZBheader = lines[i].replace(/^(head[er]{0,2}|subje[ck]t)\s*:*\s*/i, "").trim();
-		// if header is empty check next line
-		if (NZBheader === "" && i + 1 < lines.length && lines[i+1].trim() !== "") {
-			NZBheader = lines[++i].trim(); // make sure to skip next line since we already consumed it
-		}
+// test if the selection contains a NZB file name in the format of nzbfilename{{password}}
+// we first assume that the NZB file name is on its own line
+if (/^(.*){{(.*?)}}/m.test(NZBselection)) {
+	// set the title and password according to the NZB filename
+	NZBtitle = NZBselection.match(/^(.*){{(.*?)}}/m)[1]; 
+	NZBpassword = NZBselection.match(/^(.*){{(.*?)}}/m)[2];
+	// check if maybe there is nevertheless a leading description and remove it from the title
+	// assuming that the leading description includes the word NZB and ends with a colon
+	if (/.*nzb.*:\s*/i.test(NZBtitle)) {
+		NZBtitle = NZBtitle.replace(/.*nzb.*:\s*/i, "");
 	}
-	if (lines[i].trim().match(/^passwor[td]\s*:*\s*/i)) {
-		NZBpassword = lines[i].replace(/^passwor[td]\s*:*\s*/i, "").trim();
-		// if password is empty check next line
-		if (NZBpassword === "" && i + 1 < lines.length && lines[i+1].trim() !== "") {
-			NZBpassword = lines[++i].trim(); // make sure to skip next line since we already consumed it
-		}
+}
+// if no NZB file name was found the title and password have to be set by another way
+else {
+	// in this case simply set title to the first line of the selection
+	NZBtitle = NZBselection.split("\n")[0];
+
+	// test if the selection contains a description for the password starting with some common words used for and ending with a colon
+	if (/^(passwor[td]|pw).*:\s*/im.test(NZBselection)) {
+		// set the password to the text after the description
+		// we search for any text until we find it and then get all of it until the next line break
+		// like this we will find the password either if placed on the same line or if placed on the next line
+		// we also take care of if the description is used twice (e.g. before the hidden tag and in the hidden tag again)
+		NZBpassword = NZBselection.match(/^((passwor[td]|pw).*:\s*)+(\S.*\S)[\n|\r]/im)[3];
 	}
 }
 
-// get the groups if any and join them to a string with line breaks
-if (NZBselection.match(/alt\.[a-z0-9.+_-]*/ig)) {
-	NZBgroup = NZBselection.match(/alt\.[a-z0-9.+_-]*/ig).join('\n');
+// test if the selection contains usenet group names in the format alt.xyz or a.b.xyz
+if (/\b(alt|a\.b)(\.[a-z0-9.+_-]*\b)/i.test(NZBselection)) {
+	// get all usenet group names in the format alt.xyz or a.b.xyz and join them to a string with line breaks
+	NZBgroup = NZBselection.match(/\b(alt|a\.b)(\.[a-z0-9.+_-]*\b)/ig).join("\n");
+	// if the abbreviation a.b. is used replace it by alt.binaries.
+	NZBgroup = NZBgroup.replace(/^a\.b\./img, "alt.binaries.");
 }
 
 // generate the NZBlink
-NZBlink = generateNZBLink(NZBtitle, NZBheader, NZBpassword, NZBgroup)
+NZBlink = generateNZBLink(NZBtitle, NZBheader, NZBpassword, NZBgroup);
 
 // start the overlay
 $.prompt({
@@ -52,7 +70,7 @@ $.prompt({
 		html: '<label class="linker" for="NZBtitle">Titel:</label><span class="linker"><input type="text" id="NZBtitle" name="ftitle" value="' + NZBtitle + '"/></span><br/>'
 			+ '<label class="linker" for="NZBheader">Header:</label><span class="linker"><input type="text" id="NZBheader" name="fhead" value="' + NZBheader + '"/></span><br/>'
 			+ '<label class="linker" for="NZBpassword">Passwort:</label><span class="linker"><input type="text" id="NZBpassword" name="fpass" value="' + NZBpassword + '"/></span><br/>'
-			+ '<label class="linker" for="NZBgroup">Gruppe(n) (eine Gruppe pro Linie):</label><span class="linker"><textarea id="NZBgroup" name="fgroup" rows="4">' + NZBgroup + '</textarea></span><br/>'
+			+ '<label class="linker" for="NZBgroup">Gruppe(n) (eine Gruppe pro Zeile):</label><span class="linker"><textarea id="NZBgroup" name="fgroup" rows="4">' + NZBgroup + '</textarea></span><br/>'
 			+ '<label class="linker" for="NZBselection">Markierter Text:</label><span class="linker"><textarea id="NZBselection" rows="7">' + NZBselection + '</textarea></span><br/>'
 			+ '<label class="linker" for="NZBlink">NZBLink:</label><span class="linker"><input type="text" id="NZBlink" name="fnzblink" value="' + NZBlink + '"/></span>',
 		buttons: {
@@ -76,19 +94,19 @@ $.prompt({
 });
 
 // jQuery functions to check if inputs are changed and call the updateNZBLink function
-$("#NZBtitle").on('change keydown paste input', function(){
+$("#NZBtitle").on("change keydown paste input", function(){
 	updateNZBLink();
 });
 
-$("#NZBheader").on('change keydown paste input', function(){
+$("#NZBheader").on("change keydown paste input", function(){
 	updateNZBLink();
 });
 
-$("#NZBpassword").on('change keydown paste input', function(){
+$("#NZBpassword").on("change keydown paste input", function(){
 	updateNZBLink();
 });
 
-$("#NZBgroup").on('change keydown paste input', function(){
+$("#NZBgroup").on("change keydown paste input", function(){
 	updateNZBLink();
 });
 
@@ -100,15 +118,15 @@ function updateNZBLink() {
 // function to generate the NZBLink
 function generateNZBLink(title, header, password, group) {
 	// sanitize title
-	var cleanTitle = title.normalize('NFD').replace(/[^\x20-\x7E]/g, "").replace(/[/\\?%*:|"<>]/g, "");
+	var cleanTitle = title.normalize("NFD").replace(/[^\x20-\x7E]/g, "").replace(/[/\\?%*:|"<>]/g, "");
 
 	var groups = "";
 		
 	// repeat the 'g' parameter for each mentioned group if any
 	if (group) {
-		group = group.split('\n');
+		group = group.split("\n");
 		for (i = 0; i < group.length; i++) {
-			groups = groups + "&g=" + encodeURIComponent(group[i]);
+			groups = groups + "&g=" + encodeURIComponent(group[i].replace(/^a\.b\./img, "alt.binaries.")); // if the abbreviation a.b. is used (e.g. if entered manually in the groups field) replace with alt.binaries.
 		}
 	}
 
@@ -116,21 +134,21 @@ function generateNZBLink(title, header, password, group) {
 	return "nzblnk:?t=" + encodeURIComponent(cleanTitle)
 						+ "&h=" + encodeURIComponent(header)
 						+ "&p=" + encodeURIComponent(password)
-						+ groups
+						+ groups;
 }
 
 // function to get the html from the selection
  function getSelectionHtml() {
-    var html = "";
-    if (typeof window.getSelection != "undefined") {
-        var sel = window.getSelection();
-        if (sel.rangeCount) {
-            var container = document.createElement("div");
-            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                container.appendChild(sel.getRangeAt(i).cloneContents());
-            }
-            html = container.innerHTML;
-        }
-    }
-    return '<div>' + html + '</div>'; // encapsulate with a div container otherwise jQuery.text() will remove plain text
+	var html = "";
+	if (typeof window.getSelection != "undefined") {
+		var sel = window.getSelection();
+		if (sel.rangeCount) {
+			var container = document.createElement("div");
+			for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+				container.appendChild(sel.getRangeAt(i).cloneContents());
+			}
+			html = container.innerHTML;
+		}
+	}
+	return "<div>" + html + "</div>"; // encapsulate with a div container otherwise jQuery.text() will remove plain text
 }
