f38975a4814787a36ea09b5d486f6db1d2936a5b
Tensai75 <34959627+Tensai75@users.noreply.github.com>
2018-03-12 15:43:06 +0100

    Small improvements to the selection analyzing routine

--- overlay.js
+++ overlay.js
@@ -11,18 +11,18 @@ var NZBselection = "",
 NZBselection = getSelectionHtml().replace(/<input.+?value\s*?=\s*?['"](.*?)['"].*?>/ig, "\n$1\n");
 
 // add some line breaks to some ending tags to avoid text rendered on different lines to be joined in one line
-NZBselection = NZBselection.replace(/(<\/div>|<\/span>|<\/p>|<\/td>|<\/li>)/ig, "$1\n");
+NZBselection = NZBselection.replace(/(<\/div>|<\/p>|<\/td>|<\/li>)/ig, "$1\n");
 
 // remove all tags, blank lines and leading/trailing spaces
 NZBselection = jQuery(NZBselection).text().replace(/^\s*([\s|\S]*?)\s*?$/mg, "$1");
 
 // test if the selection contains a description for the header starting with some common words used for and ending with a colon
-if (/^.*((header|subje[ck]t|betreff).*:\s*)+(\S.*\S)[\n|\r]/im.test(NZBselection)) {
+if (/^.*((header|subje[ck]t|betreff).*:\s*)+(\S.*\S)$/im.test(NZBselection)) {
 	// set the header to the text after the description
 	// we search for any text until we find it and then get all of it until the next line break
 	// like this we will find the header information either if placed on the same line or if placed on the next line
 	// we also take care of if the description is used twice (e.g. before the hidden tag and in the hidden tag again)
-	NZBheader = NZBselection.match(/^.*((header|subje[ck]t|betreff).*:\s*)+(\S.*\S)[\n|\r]/im)[3];
+	NZBheader = NZBselection.match(/^.*((header|subje[ck]t|betreff).*:\s*)+(\S.*\S)$/im)[3];
 }
 
 // test if the selection contains a NZB file name in the format of nzbfilename{{password}}
@@ -43,12 +43,12 @@ else {
 	NZBtitle = NZBselection.split("\n")[0];
 
 	// test if the selection contains a description for the password starting with some common words used for and ending with a colon
-	if (/^.*((passwor[td]|pw|pass).*:\s*)+(\S.*\S)[\n|\r]/im.test(NZBselection)) {
+	if (/^.*((passwor[td]|pw|pass).*:\s*)+(\S.*\S)$/im.test(NZBselection)) {
 		// set the password to the text after the description
 		// we search for any text until we find it and then get all of it until the next line break
 		// like this we will find the password either if placed on the same line or if placed on the next line
 		// we also take care of if the description is used twice (e.g. before the hidden tag and in the hidden tag again)
-		NZBpassword = NZBselection.match(/^.*((passwor[td]|pw|pass).*:\s*)+(\S.*\S)[\n|\r]/im)[3];
+		NZBpassword = NZBselection.match(/^.*((passwor[td]|pw|pass).*:\s*)+(\S.*\S)$/im)[3];
 	}
 }
 
@@ -178,7 +178,7 @@ chrome.storage.sync.get("NZBsettings", function(obj) {
 			for (var i = 0, len = sel.rangeCount; i < len; ++i) {
 				container.appendChild(sel.getRangeAt(i).cloneContents());
 			}
-			html = container.innerHTML;
+			html = container.outerHTML;
 		}
 	}
 	return "<div>" + html + "</div>"; // encapsulate with a div container otherwise jQuery.text() will remove plain text
