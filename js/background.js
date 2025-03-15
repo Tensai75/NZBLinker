9badc06f5d1e08ee3210505a7b9373f19b568eea
unknown <dannhardt@googlemail.com>
2025-03-15 16:44:36 +0100

    ported to manifest v3

--- js/background.js
+++ js/background.js
@@ -1,35 +1,47 @@
+// Hier müssen Sie die Event-Listener für die Context Menüs und die Browser Action registrieren
 chrome.contextMenus.create({
-    id: "NZBLContextMenu",
-    title: chrome.i18n.getMessage('extNZBCreateLink'),
-    contexts: ["selection"]
+  id: "NZBLContextMenu",
+  title: chrome.i18n.getMessage('extNZBCreateLink'),
+  contexts: ["selection"]
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (tab) {
-		chrome.tabs.insertCSS(tab.id, {
-            file: "/css/jquery.modal.css"
+  if (tab) {
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ["/css/jquery.modal.css"]
+    }).then(() => {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["/js/jquery-3.5.1.js"]
+      }).then(() => {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["/js/jquery.modal.js"]
+        }).then(() => {
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["/js/content.js"]
+          });
         });
-		chrome.tabs.executeScript(tab.id, { file: "/js/jquery-3.5.1.js" }, function() {
-            chrome.tabs.executeScript(tab.id, { file: "/js/jquery.modal.js" }, function() {
-                chrome.tabs.executeScript(tab.id, { file: "/js/content.js" });
-            });
-        });
-	};
+      });
+    });
+  }
 });
 
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.windows.getCurrent(function(win) {
-        var width = 540;
-        var height = 680;
-        var left = ((win.width / 2) - (width / 2)) + win.left;
-        var top = ((screen.height / 2) - (height / 2));
-        chrome.windows.create({
-            url: '/html/popup.html',
-            width: width,
-            height: height,
-            top: Math.round(top),
-            left: Math.round(left),
-            type: 'popup'
-        });
+chrome.action.onClicked.addListener(function() {
+  chrome.windows.getCurrent(function(win) {
+    var width = 540;
+    var height = 680;
+    var left = ((win.width / 2) - (width / 2)) + win.left;
+    var top = ((screen.height / 2) - (height / 2));
+    chrome.windows.create({
+      url: '/html/popup.html',
+      width: width,
+      height: height,
+      top: Math.round(top),
+      left: Math.round(left),
+      type: 'popup'
     });
+  });
 });
